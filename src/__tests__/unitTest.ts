19a43724877ee424dc1654113bb48ce591c6a28c
Onitsiky <hei.onitsiky@gmail.com>
2023-12-17 22:45:14 +0300

    chore: refactor tests

--- src/__tests__/unitTest.ts
+++ src/__tests__/unitTest.ts
@@ -1,27 +1,30 @@
 import { add, getRiceCookersList, changeState, getRiceCookerById, startCooking } from '../main/feature/actions/riceCookersAction'
 import { RiceCooker } from "../main/model/RiceCooker";
+import { describe, test, expect } from '@jest/globals';
 
-test('Add new rice cooker to list', () => {
-    add(1, true);
-    add(2, false);
+describe('Test rice cooker action', () => {
+    test('Add new rice cooker to list', () => {
+        add(1, true);
+        add(2, false);
 
-    expect(getRiceCookersList().length == 2)
-})
+        expect(getRiceCookersList().length == 2)
+    })
 
-test('Change rice cooker state', () => {
-    add(1, false);
-    let expected = new RiceCooker(1, true);
-    expected.isCooking = true;
-    expected.isPlugged = true;
+    test('Change rice cooker state', () => {
+        add(1, false);
+        let expected = new RiceCooker(1, true);
+        expected.isCooking = true;
+        expected.isPlugged = true;
 
-    changeState(1, "isOperational", true);
-    changeState(1, "isPlugged", true);
-    changeState(1, "isCooking", true);
-    expect(getRiceCookerById(1)).toStrictEqual(expected);
-})
+        changeState(1, "isOperational", true);
+        changeState(1, "isPlugged", true);
+        changeState(1, "isCooking", true);
+        expect(getRiceCookerById(1)).toStrictEqual(expected);
+    })
 
-test('Rice cooker started cooking', () => {
-    add(3, true);
-    startCooking(3);
-    expect(getRiceCookerById(3)?.isCooking).toBe(true);
+    test('Rice cooker started cooking', () => {
+        add(3, true);
+        startCooking(3);
+        expect(getRiceCookerById(3)?.isCooking).toBe(true);
+    })
 })
