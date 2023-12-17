f910f6edcb8a23d50ce46ecbf9535eeae96c030d
Onitsiky <hei.onitsiky@gmail.com>
2023-12-17 21:09:22 +0300

    feat: tests

---
+++ src/test/rice_cooker_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require_relative '../main/feature/handling/handle_rc'
+require_relative '../main/model/rice_cooker'
+
+class Test < Minitest::Test
+    def test_add_rice_cooker
+        RcHandler.add(1, true)
+        RcHandler.add(2, false)
+        assert(2 == RcHandler.rc_list.size)
+    end
+
+    def test_change_state
+        RcHandler.change_state(2, "is_operational", true)
+        RcHandler.change_state(2, "is_cooking", true)
+        assert(RcHandler.get_rc(2).is_operational == true)
+        assert(RcHandler.get_rc(2).is_cooking == true)
+
+        RcHandler.change_state(1, "is_plugged", true)
+        assert(RcHandler.get_rc(1).is_plugged == true)
+    end
+end
