39cf8bf7649ff8b124c23999fc5ba57798cf0af9
Onitsiky <hei.onitsiky@gmail.com>
2023-12-18 22:32:35 +0300

    feat: add tests

--- app/src/test/kotlin/my/rice/cooker/AppTest.kt
+++ app/src/test/kotlin/my/rice/cooker/AppTest.kt
@@ -3,12 +3,37 @@
  */
 package my.rice.cooker
 
+import add
+import changeState
+import getRC
+import getRCList
 import kotlin.test.Test
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class AppTest {
-    @Test fun appHasAGreeting() {
-        val classUnderTest = App()
-        assertNotNull(classUnderTest.greeting, "app should have a greeting")
+    @Test fun addNewRiceCooker() {
+        add(1, true);
+        println(getRCList().size)
+        assertTrue(2 == getRCList().size);
+    }
+
+    @Test fun getRiceCookerById() {
+        var actual = getRC(2);
+        assertNotNull(actual);
+        assertTrue(actual.id == 2);
+    }
+
+    @Test fun changeRiceCookerState() {
+        add(2, false);
+        changeState(2, "isOperational", true);
+        changeState(2, "isCooking", true);
+        changeState(2, "isPlugged", true);
+        var actual = getRC(2);
+
+        assertNotNull(actual);
+        assertTrue(actual.isOperational);
+        assertTrue(actual.isCooking);
+        assertTrue(actual.isPlugged);
     }
 }
